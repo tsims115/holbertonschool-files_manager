1aaf77bd744133ed27372649ccd2432758aba200
Montywilliams <3650@holbertonschool.com>
2022-11-17 14:24:42 -0600

    set up the class to handle user creating new posts

---
+++ controllers/UsersController.js
@@ -0,0 +1,7 @@
+const Redis = require('../utils/redis');
+const Mongo = require('../utils/mongodb');
+const sha1 = require('../utils/sha1');
+const mongodb = require('mongodb');
+
+//here we create the functionality that the post rout points to in index.js
+
