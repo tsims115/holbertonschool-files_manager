517a8c724c8c00ffc5c236a027db0bdaf141bf90
Montywilliams <3650@holbertonschool.com>
2022-11-17 14:28:10 -0600

    create UsersController class

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,5 +3,8 @@ const Mongo = require('../utils/mongodb');
 const sha1 = require('../utils/sha1');
 const mongodb = require('mongodb');
 
-//here we create the functionality that the post rout points to in index.js
+class UsersController {
 
+}
+
+module.exports = UsersController;
