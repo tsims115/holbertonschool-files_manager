24bcb11c38f4678abfbab1e917525e7293446454
Montywilliams <3650@holbertonschool.com>
2022-11-17 18:13:10 -0600

    post new function will point to our end point and check the response

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,15 @@ const sha1 = require('../utils/sha1');
 const mongodb = require('mongodb');
 
 class UsersController {
-
+    postNew(request, response) {
+        const {email, password } = request.body;
+        if (!email) {
+            response.status(400).json({error: 'Missing email'});
+        }
+        if (!password) {
+			response.status(400).json({error: 'Missing password'});
+		}
+    }
 }
 
 module.exports = UsersController;
