1c24b97a7614de53a1ec9cbf9d1ff0a781e60b56
tsims115 <tsims115@yahoo.com>
2022-11-21 20:37:05 +0000

    Adding task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,19 +7,19 @@ class UsersController {
     static async postNew(request, response) {
         const {email, password } = request.body;
         if (!email) {
-            return response.status(400).json({error: 'Missing email'});
+            return response.status(400).json({ error: 'Missing email' });
         }
         if (!password) {
-			return response.status(400).json({error: 'Missing password'});
+			return response.status(400).json({ error: 'Missing password' });
 		}
         if (await Mongo.users.findOne({email})) {
-            return response.status(400).json({error: 'Already exist'});
+            return response.status(400).json({ error: 'Already exist' });
         }
         const userVar = await Mongo.users.insertOne({
             email,
-            passsword: sha1(password)
+            password: sha1(password)
         });
-        return response.status(201).json({id: userVar.insertedId, email});
+        return response.status(201).json({ id: userVar.insertedId, email });
     }
 }
 
