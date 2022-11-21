d297e749eebe3ff3b96efca403814a38384b718a
tsims115 <tsims115@yahoo.com>
2022-11-21 20:50:05 +0000

    Adding task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,26 +1,24 @@
-const Redis = require('../utils/redis');
-const Mongo = require('../utils/db');
 const sha1 = require('sha1');
-const mongodb = require('mongodb');
+const Mongo = require('../utils/db');
 
 class UsersController {
-    static async postNew(request, response) {
-        const {email, password } = request.body;
-        if (!email) {
-            return response.status(400).json({ error: 'Missing email' });
-        }
-        if (!password) {
-			return response.status(400).json({ error: 'Missing password' });
-		}
-        if (await Mongo.users.findOne({email})) {
-            return response.status(400).json({ error: 'Already exist' });
-        }
-        const userVar = await Mongo.users.insertOne({
-            email,
-            password: sha1(password)
-        });
-        return response.status(201).json({ id: userVar.insertedId, email });
+  static async postNew(request, response) {
+    const { email, password } = request.body;
+    if (!email) {
+      return response.status(400).json({ error: 'Missing email' });
     }
+    if (!password) {
+      return response.status(400).json({ error: 'Missing password' });
+    }
+    if (await Mongo.users.findOne({ email })) {
+      return response.status(400).json({ error: 'Already exist' });
+    }
+    const userVar = await Mongo.users.insertOne({
+      email,
+      password: sha1(password),
+    });
+    return response.status(201).json({ id: userVar.insertedId, email });
+  }
 }
 
 module.exports = UsersController;
