f4195de22af6616ccaff0c21781abad1fdcd2b60
tsims115 <tsims115@yahoo.com>
2022-11-22 04:44:48 +0000

    Adding task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -67,6 +67,78 @@ class FilesController {
       id: newFile.insertedId, userId, name, type, isPublic, parentId,
     });
   }
+  static async getShow(request, response) {
+    const userId = new mongodb.ObjectId(request.params.id);
+    const token = request.headers['X-token'];
+    const key = `auth_${token}`;
+    const redisUser = await Redis.get(authToken);
+    if (!redisUser) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+    const file = await Mongo.files.findOne({ _id: userId });
+    if (!file) {
+      return response.status(404).json({ error: 'Not found' });
+    }
+    if (redisUser !== file.userId) {
+      return response.status(404).json({ error: 'Not found' });
+    }
+    return response.status(200).json({
+      id: file.id, userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
+  static async getIndex(request, response) {
+    const token = request.headers['X-token'];
+    const key = `auth_${token}`;
+    const redisUser = await Redis.get(authToken);
+    if (!redisUser) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+    const { page = 0, parentId = 0} = request.query;
+    let file = "file";
+    if (parentId !== 0) {
+      file = await Mongo.files.findOne({ parentId: parentId });
+      const files = Mongo.files.aggregate([
+        { $match: { parentId: new mongodb.ObjectId(parentId) } },
+        { $skip: page * 20 },
+        { $limit: 20 },
+      ]).toArray();
+      if (files === []) {
+        return response.status(200).json([]);
+      }
+      let fileList = [];
+      for ( let i in files) {
+        fileList.push({
+          id: i._id,
+          userId: i.userId,
+          name: i.name,
+          type: i.type,
+          isPublic: i.isPublic,
+          parentId: i.parentId,
+        });
+      }
+      return resonse.status(200).json(fileList);
+    }
+    const files = await Mongo.files.aggregate([
+      { $match: { userId: new mongodb.ObjectId(new mongodb.ObjectId(redisUser)) } },
+      { $skip: page * 20 },
+      { $limit: 20 },
+    ]).toArray();
+    for ( let i in files) {
+      fileList.push({
+        id: i._id,
+        userId: i.userId,
+        name: i.name,
+        type: i.type,
+        isPublic: i.isPublic,
+        parentId: i.parentId,
+      });
+    }
+    return resonse.status(200).json(fileList);
+  }
 }
 
 module.exports = FilesController;
