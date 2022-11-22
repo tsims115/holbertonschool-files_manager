9b0875670fbc2a11d210fd8629415ffbc5d35c85
tsims115 <tsims115@yahoo.com>
2022-11-22 00:08:10 +0000

    Adding task 4

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,24 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const sha1 = require('sha1');
 const Mongo = require('../utils/db');
 const Redis = require('../utils/redis');
-import { v4 as uuidv4 } from 'uuid';
 
 class AuthController {
   static async getConnect(request, response) {
-    let Authorization = request.headers['authorization'];
-    Authorization = Authorization.split(' ')[1];
-    Authorization = Buffer.from(Authorization, "base64");
+    let Authorization = request.headers.authorization.split(' ')[1];
+    Authorization = Buffer.from(Authorization, 'base64');
     Authorization = Authorization.toString('utf-8').split(':');
     const email = Authorization[0];
-    const password = Authorization[1];
+    let password = Authorization[1];
+    if (password) {
+      password = sha1(password);
+    } else {
+      password = null;
+    }
     const user = await Mongo.users.findOne({
       email,
-      password: password ? sha1(password) : null,
+      password,
     });
     if (!user) {
       return response.status(401).json({ error: 'Unauthorized' });
@@ -25,7 +30,7 @@ class AuthController {
   }
 
   static async getDisconnect(request, response) {
-    let token = request.headers['x-token'];
+    const token = request.headers['x-token'];
     const userSession = await Redis.get(`auth_${token}`);
     if (!userSession) {
       return response.status(401).json({ error: 'Unauthorized' });
